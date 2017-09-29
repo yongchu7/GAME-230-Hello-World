e61da5efd954ba8c5f4bfb3bedf8ed4709fd333b
zjhzcyt <zjhzcyt@GMAIL.COM>
2017-09-28 21:06:50 -0700

    Hello, world!
    This is the basic program about how to print hello world and the words I
    want to say in the screen.

---
+++ hello-world/hello-world/Source.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	cout << "Hello, World!" << endl;
+	cout << "Alright, evil within 2 will release next next week, I just can't wait lol." << endl;
+	system("pause");
+	return 0;
+
+}
